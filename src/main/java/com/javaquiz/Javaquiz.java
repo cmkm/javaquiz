46be17d501bb94496a78c09e7c2d2123848d85b3
James <James@130.254.205.79>
2017-01-28 17:38:51 -0500

    added mysql dependencies and queried chapters to populate observable list

--- src/main/java/com/javaquiz/Javaquiz.java
+++ src/main/java/com/javaquiz/Javaquiz.java
@@ -7,7 +7,14 @@ import com.javaquiz.views.SectionView;
 import com.gluonhq.charm.glisten.application.MobileApplication;
 import com.gluonhq.charm.glisten.layout.layer.SidePopupView;
 import com.gluonhq.charm.glisten.visual.Swatch;
+import com.javaquiz.model.Chapter;
+import com.javaquiz.model.Chapters;
 import com.javaquiz.views.QuestionView;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
@@ -20,16 +27,33 @@ public class Javaquiz extends MobileApplication {
     public static final String SECTION_VIEW = "Section View";
     public static final String QUESTION_VIEW = "Question View";
     public static final String MENU_LAYER = "Side Menu";
-    
+
     @Override
     public void init() {
+        initializeDB();
         addViewFactory(PRIMARY_VIEW, () -> new PrimaryView(PRIMARY_VIEW).getView());
         addViewFactory(SECONDARY_VIEW, () -> new SecondaryView(SECONDARY_VIEW).getView());
         addViewFactory(CHAPTER_VIEW, () -> new ChapterView(CHAPTER_VIEW).getView());
         addViewFactory(SECTION_VIEW, () -> new SectionView(SECTION_VIEW).getView());
         addViewFactory(QUESTION_VIEW, () -> new QuestionView(QUESTION_VIEW).getView());
-        
+
         addLayerFactory(MENU_LAYER, () -> new SidePopupView(new DrawerManager().getDrawer()));
+    }
+
+    public void initializeDB() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver loaded");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/javaquiz", "james", "abc123");
+            Statement stmt = connection.createStatement();
+            String getChapters = "Select * from chapter";
+            ResultSet rset = stmt.executeQuery(getChapters);
+            while (rset.next()) {
+                Chapters.chapterList.add(new Chapter(rset.getInt(1), String.valueOf(rset.getObject(2))));
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Failed");
+        }
     }
 
     @Override
